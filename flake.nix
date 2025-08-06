c5686f74ec41b230fea7c7c42dd754aed8e320b8
F. Emerson <t@utologi.cc>
2025-08-06 20:06:46 -0300

    wip

--- flake.nix
+++ flake.nix
@@ -1,6 +1,16 @@
 {
   description = "fabricte.st - eff's homelab";
 
+  nixConfig = {
+    # TODO(eff): Add fabrictest cache.
+    extra-substituters = [
+      "https://devenv.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
+
   inputs.clan-core = {
     url = "https://git.clan.lol/clan/clan-core/archive/main.tar.gz";
     inputs = {
@@ -31,17 +41,8 @@
   };
 
   inputs.systems = {
-    url = "github:nix-systems/default";
-  };
-
-  nixConfig = {
-    # TODO(eff): Add fabrictest cache.
-    extra-substituters = [
-      "https://devenv.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-    ];
+    url = "path:./flake.systems.nix";
+    flake = false;
   };
 
   outputs =
@@ -50,16 +51,15 @@
       devenv,
       flake-parts,
       nixpkgs,
-      systems,
       ...
     }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    flake-parts.lib.mkFlake { inherit inputs; } rec {
       imports = [
         clan-core.flakeModules.default
         devenv.flakeModules.default
       ];
 
-      systems = import systems;
+      systems = import inputs.systems;
 
       clan = {
         meta.name = "fabrictest";
@@ -80,7 +80,7 @@
           };
 
           devenv.shells.default = {
-            name = "fabrictest";
+            inherit (clan.meta) name;
 
             enterShell = '''';
 
@@ -95,7 +95,14 @@
 
             #tasks
 
-            languages.terraform.enable = true;
+            languages.terraform = {
+              enable = true;
+              package = pkgs.opentofu.withPlugins (p: [
+                p.cloudflare
+                p.migadu
+                p.random
+              ]);
+            };
 
             processes.terraform-backend-git = rec {
               exec = getExe pkgs.terraform-backend-git;
@@ -112,59 +119,55 @@
               push = "fabrictest";
             };
 
-            git-hooks = {
-              hooks.treefmt = {
-                enable = true;
-                settings.formatters = with pkgs; [
-                  # *
-                  keep-sorted
+            git-hooks.hooks.treefmt = {
+              enable = true;
+              settings.formatters = with pkgs; [
+                # *
+                keep-sorted
 
-                  # JSON
-                  jsonfmt
+                # JSON
+                jsonfmt
 
-                  # Markdown
-                  mdformat
-                  mdsh
+                # Markdown
+                mdformat
+                mdsh
 
-                  # Nix
-                  deadnix
-                  nixfmt-rfc-style
-                  statix
+                # Nix
+                deadnix
+                nixfmt-rfc-style
+                statix
 
-                  # Shell
-                  shfmt
+                # Shell
+                shfmt
 
-                  # TOML
-                  taplo
+                # TOML
+                taplo
 
-                  # YAML
-                  yamlfmt
-                  yamllint
-                ];
-              };
-
-              # TODO(eff): Should we add linters to treefmt as well?
-
-              hooks.actionlint.enable = true;
-
-              hooks.editorconfig-checker.enable = true;
-
-              hooks.shellcheck.enable = true;
-
-              # TODO(eff): Add a new hook for zizmor.
+                # YAML
+                yamlfmt
+                yamllint
+              ];
             };
+
+            # TODO(eff): Should we add linters to treefmt as well?
+
+            git-hooks.hooks.actionlint = {
+              enable = true;
+            };
+
+            git-hooks.hooks.editorconfig-checker = {
+              enable = true;
+            };
+
+            git-hooks.hooks.shellcheck = {
+              enable = true;
+            };
+
+            # TODO(eff): Add a new hook for zizmor.
 
             enterTest = '''';
 
-            overlays = [
-              (_final: prev: {
-                terraform = prev.opentofu.withPlugins (p: [
-                  p.cloudflare
-                  p.migadu
-                  p.random
-                ]);
-              })
-            ];
+            # overlays = [ ];
 
             apple.sdk = null;
 
